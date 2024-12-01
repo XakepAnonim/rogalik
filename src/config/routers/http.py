fea98f5839cc60b557f319f4006386885d8d0c12
xakepanonim <dimka.fisenko@mail.ru>
2024-12-01 19:08:29 +0300

    try authentication logic

--- src/config/routers/http.py
+++ src/config/routers/http.py
@@ -9,8 +9,10 @@ from fastapi import Request
 from fastapi import status
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
+from config.log_tools import logger
 from config.settings import settings
 from logic.utils.auth_utils import create_token
 from logic.utils.auth_utils import jwt_authenticated
@@ -18,7 +20,10 @@ from models.client.player.base import PlayerCreateModel
 from models.client.player.base import PlayerLoginModel
 from models.db.base import Player
 from models.db.dependencies import get_db
+from models.exceptions import AuthenticationError
 from models.exceptions import HTTPError
+from models.repository.player.base import PlayerConfigurationModel
+from models.repository.player.base import PlayerRepositoryModel
 
 main_router = APIRouter()
 
@@ -27,30 +32,36 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @main_router.post("/login")
-async def login(user: PlayerLoginModel, db: Session = Depends(get_db)) -> dict:
+async def login(user: PlayerLoginModel, db: AsyncSession = Depends(get_db)) -> dict:
     """
     Роутер для входа в игру.
     """
-    existing_user = db.query(Player).filter_by(username=user.username).first()
-    if not existing_user:
+    get_user = await db.execute(select(Player).where(Player.email == user.email))
+    existing_user = get_user.scalar()
+    if not existing_user or not pwd_context.verify(user.password, existing_user.password):
         raise HTTPError(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid credentials")
 
-    if not pwd_context.verify(user.password, existing_user.password):
-        raise HTTPError(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid credentials")
-
-    token = create_token({"sub": existing_user.username})
+    try:
+        token = create_token({"sub": existing_user.username})
+    except Exception as e:
+        logger.info(f"Failed to login user: {e}")
+        raise AuthenticationError(str(e)) from e
 
     return {"access_token": token, "type": "bearer"}
 
 
 @main_router.post("/register")
-async def register(user: PlayerCreateModel, db: Session = Depends(get_db)) -> dict:
+async def register(user: PlayerCreateModel, db: AsyncSession = Depends(get_db)) -> dict:
     """
     Роутер для регистрации.
     """
-    existing_user = db.query(Player).filter_by(email=user.email).first()
-    if existing_user:
+    existing_user_by_email = await db.execute(select(Player).where(Player.email == user.email))
+    if existing_user_by_email.scalar() is not None:
         raise HTTPError(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+
+    existing_user_by_username = await db.execute(select(Player).where(Player.username == user.username))
+    if existing_user_by_username.scalar() is not None:
+        raise HTTPError(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already taken")
 
     try:
         hashed_password = pwd_context.hash(user.password)
@@ -61,11 +72,12 @@ async def register(user: PlayerCreateModel, db: Session = Depends(get_db)) -> di
         new_user.password = hashed_password
 
         db.add(new_user)
-        db.commit()
-        db.refresh(new_user)
+        await db.commit()
+        await db.refresh(new_user)
 
         token = create_token({"sub": new_user.username})
     except Exception as e:
+        logger.info(f"Failed to register user: {e}")
         raise HTTPError(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e)) from e
 
     return {"access_token": token, "type": "bearer"}
@@ -90,9 +102,9 @@ def logout(request: Request, token: str = Depends(oauth2_scheme)) -> dict:
         raise HTTPError(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Logout failed") from e
 
 
-@main_router.get("/me")
+@main_router.get("/me", response_model=PlayerRepositoryModel)
 @jwt_authenticated
-async def get_me(request: Request, db: Session = Depends(get_db)) -> dict:
+async def get_me(request: Request, db: AsyncSession = Depends(get_db)) -> PlayerRepositoryModel:
     """
     Маршрут для получения информации о текущем пользователе.
     """
@@ -100,15 +112,18 @@ async def get_me(request: Request, db: Session = Depends(get_db)) -> dict:
         jwt_decoded = request.state.jwt_decoded
         username = jwt_decoded["sub"]
 
-        player = db.query(Player).filter_by(username=username).first()
+        get_user = await db.execute(select(Player).where(Player.username == username))
+        player = get_user.scalars().first()
         if not player:
             raise HTTPError(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
 
-        return {
-            "username": player.username,
-            "email": player.email,
-            "characters": [char.name for char in player.characters],
-        }
+        return PlayerRepositoryModel(
+            player_configuration=PlayerConfigurationModel(
+                username=player.username,
+                email=player.email,
+            )
+        )
+
     except Exception as e:
         raise HTTPError(status_code=status.HTTP_400_BAD_REQUEST, detail="Failed to retrieve user data") from e
 
